4387b1f0199607e8695bb3f72aa4c7be75bac952
Shordal <oneholy@Hotmail.com>
2017-11-03 21:18:31 -0500

    Initial Commit

---
+++ Random Number Gen 4 DLL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Random Number Gen 4 DLL
+//
+//  Created by Steven Payne on 11/3/17.
+//  Copyright © 2017 Steven Payne. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
